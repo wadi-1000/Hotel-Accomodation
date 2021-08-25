868e004d1a5337d4ecd445b13557a5fe313d2efb
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-08-25 18:55:17 +0300

    Created function to process json data to list of objects

--- app/requests.py
+++ app/requests.py
@@ -24,6 +24,28 @@ def get_exercises():
             exercise_results = process_exercises(exercise_results_list)
 
     return exercise_results
-        
 
+def exercise_results(exercise_list):
+     '''
+    Function that processes the exercise result and transform them to a list of objects
+
+    Args:
+        exrcise_list: A list of dictionaries that contain exercise details
+
+    Returns :
+        exercise_results: A list of exercise objects
+    '''
+    exercise_results =[]
+    for exercise_item in exercise_list:
+        id = exercise_item.get('id')
+        name = exercise_item.get('name')
+        description = exercise_item.get('description')
+        category = exercise_item.get('category')
+        muscles = exercise_item.get('muscles')
+        equipment = exercise_item.get('equipment')
+
+        exercise_object =ExerciseInfo(id,name,description,category,muscles,equipment)
+        exercise_results.append(exercise_object)
+    
+    return exercise_results
 
